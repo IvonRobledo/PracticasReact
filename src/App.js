d849af7819e4720e5260b48c8049ee5812adc5a4
Ivon Olivares <iolivares@lenovo.com>
2022-10-05 17:05:36 -0500

    limpiar App

--- src/App.js
+++ src/App.js
@@ -5,25 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (  
-      <Fragment>
-        {/*<PresentationCard/>
-        <Contador/>
-      <ContadorClase/>
-      <Layout header="Header, llega en su propia prop">
-        Este texto debe estar en un div chiquito en el centro, a través de la prop de children :)
-      </Layout>
-      <Container/>
-      <TodoContainer/>
-      <Car/>
-      <ProductosContainer/>*/}
       <Router>
-        <div><Header/></div>
-          
-         
-
-        </Router>
-    </Fragment>
-    
+          <Header/>
+      </Router>
   );
 }
 
